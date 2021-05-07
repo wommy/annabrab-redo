16e25a6ab1ac0e50e074c95aa61edba6f65bff24
wommy <tom@technomad.media>
2021-05-07 05:43:55 -0400

    feat: art, artwork WIP

---
+++ _data/art.js
@@ -0,0 +1,200 @@
+module.exports = [
+	{
+		name: "scrub",
+		img: "https://res.cloudinary.com/wommy/image/upload/v1613000980/annabrab/IMG_1239_l8me54.jpg",
+		published: "April, 2020",
+		description: [
+			[
+				"spray paint",
+				"enamel",
+				"acrylic",
+			],
+			"on canvas",
+		],
+		width: 1,
+	},
+	{
+		name: "trash painting 2020",
+		img: "https://res.cloudinary.com/wommy/image/upload/v1613000980/annabrab/2AE30EB5-07AD-46E6-9559-9AD42F52F71E_fx1r9c.jpg",
+		published: "March, 2020",
+		description: [
+			[
+				"spray paint",
+				"enamel",
+				"pen",
+			],
+			"on canvas",
+		],
+		width: 1,
+	},
+	{
+		name: "errors III",
+		img: "https://res.cloudinary.com/wommy/image/upload/v1598373214/annabrab/3993CB33-BFF3-427C-B70C-6F80377FF5E5_vpq5rj.jpg",
+		published: "September, 2020",
+		description: "",
+		width: 2,
+	},
+	{
+		name: "defect",
+		img: "https://res.cloudinary.com/wommy/image/upload/v1595088745/annabrab/defect_vtvklr.jpg",
+		published: "",
+		description: "",
+		width: 1,
+	},
+	{
+		name: "celery",
+		img: "https://res.cloudinary.com/wommy/image/upload/v1595088751/annabrab/celery_vswuoz.jpg",
+		published: "",
+		description: "",
+		width: 1,
+	},
+	{
+		name: "trash",
+		img: "https://res.cloudinary.com/wommy/image/upload/v1595088748/annabrab/trash_xwobvu.jpg",
+		published: "",
+		description: "",
+		width: 1,
+	},
+	{
+		name: "barely sipped",
+		img: "https://res.cloudinary.com/wommy/image/upload/v1581530959/annabrab/barely_sipped_x2zw1d.jpg",
+		published: "",
+		description: [
+			[
+				"charcoal",
+				"oil",
+				"acrylic",
+			],
+			"on canvas",
+		],
+		width: 1,
+	},
+	{
+		name: "untitled monoprint",
+		img: "https://res.cloudinary.com/wommy/image/upload/v1581530986/annabrab/untitled_monoprint_zrdyg0.jpg",
+		published: "",
+		description: [
+			[
+				"pastels",
+				"ink",
+				"monoprint",
+			],
+		],
+		width: 1,
+	},
+	{
+		name: "idk",
+		img: "https://res.cloudinary.com/wommy/image/upload/v1581530952/annabrab/idk_yf82no.jpg",
+		published: "",
+		description: [
+			[
+				"collage",
+			],
+			"on canvas",
+		],
+		width: 1,
+	},
+	{
+		name: "data",
+		img: "https://res.cloudinary.com/wommy/image/upload/v1585947096/annabrab/data_yqfcpf.jpg",
+		published: "",
+		description: [
+			[
+				"acrylic",
+				"oil pastel",
+			],
+			"on linen",
+		],
+		width: 1,
+	},
+	{
+		name: "untitled",
+		img: "https://res.cloudinary.com/wommy/image/upload/v1581530983/annabrab/untitled_l0janl.jpg",
+		published: "",
+		description: [
+			[
+				"oil",
+				"paint",
+			],
+			"on canvas",
+		],
+		width: 1,
+	},
+	{
+		name: "flu",
+		img: "https://res.cloudinary.com/wommy/image/upload/v1585944269/annabrab/flu_10mb_asxlch.jpg",
+		published: "",
+		description: [
+			[
+				"oil",
+				"graphite",
+			],
+			"on canvas",
+		],
+		width: 2,
+	},
+	{
+		name: "rivers",
+		img: "https://res.cloudinary.com/wommy/image/upload/v1581530972/annabrab/rivers_pcwkup.jpg",
+		published: "",
+		description: [
+			[
+				"oil",
+			],
+			"on canvas",
+		],
+		width: 2,
+	},
+	{
+		name: "error ii",
+		img: "https://res.cloudinary.com/wommy/image/upload/v1581530970/annabrab/error_ii_k4bfdv.jpg",
+		published: "",
+		description: [
+			[
+				"oil",
+			],
+			"on canvas",
+		],
+		width: 1,
+	},
+	{
+		name: "error i",
+		img: "https://res.cloudinary.com/wommy/image/upload/v1581530971/annabrab/error_i_dq4lii.jpg",
+		published: "",
+		description: [
+			[
+				"oil",
+				"graphite",
+			],
+			"on canvas",
+		],
+		width: 1,
+	},
+	{
+		name: "markings",
+		img: "https://res.cloudinary.com/wommy/image/upload/v1581530964/annabrab/markings_lue5ot.jpg",
+		published: "",
+		description: [
+			[
+				"oil",
+			],
+			"on canvas",
+		],
+		width: 1,
+	},
+	{
+		name: "swarm",
+		img: "https://res.cloudinary.com/wommy/image/upload/v1581530985/annabrab/swarm_dwznmj.jpg",
+		published: "",
+		description: [
+			[
+				"oil",
+				"collage",
+				"paste",
+				"glass",
+			],
+			"on canvas",
+		],
+		width: 1,
+	},
+]
